b9e00066a685038a4c0e875351c34435278eb445
Nils <nilsl@student.ethz.ch>
2017-10-19 01:09:59 +0200

    Use Latex listings again

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-build_vars="-t beamer --verbose --template=resources/template.tex --latex-engine=xelatex $template_vars"
+build_vars="-t beamer --verbose --template=resources/template.tex --listings --latex-engine=xelatex $template_vars"
 
 if [[ $1 == '--latex' ]]; then
 	pandoc $build_vars -f markdown [0-9]*-*.md -o "power.tex"
